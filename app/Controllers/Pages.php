94787e532eb5e05a7cae80fbb462d8895a47bc19
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-03-21 18:04:31 +0700

    first commit

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,20 @@
+<?php  
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view ('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+
+    }
+
+
+}
+
+?>
