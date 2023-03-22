7aec1b44a0aef4b4759c6fc28916d088c0e201e4
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-03-22 01:40:10 -0700

    template

---
+++ app/Views/templates/layout.php
@@ -0,0 +1,9 @@
+<?= $this->include('templates/header'); ?>
+
+<?= $this->include('templates/sidebar'); ?>
+
+<?= $this->include('templates/navbar'); ?>
+
+<?= $this->renderSection('content'); ?>
+
+<?= $this->include('templates/footer'); ?>
